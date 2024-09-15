5a369a43f10829e03c4cd90444d4c8ae8b4e6263
mdsajed001 <mdsajed001@gmai.com>
2024-09-15 23:44:21 +0600

    login screen design fixed

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,8 +5,8 @@ export default function LoginPage() {
     <PageTitle title='Login'>
       <div className='bg-baseLight-400 relative flex h-screen items-center justify-center overflow-hidden'>
         <img
-          className='absolute -top-5 left-1/2 translate-x-[-50%] md:-top-40'
-          width={700}
+          className='absolute -top-5 left-1/2 translate-x-[-50%] md:-top-16'
+          width={600}
           src='/public/asset/education/bro.png'
           alt=''
         />
@@ -14,8 +14,8 @@ export default function LoginPage() {
           <Logo></Logo>
         </div>
         <img
-          className='absolute -bottom-5 left-1/2 translate-x-[-50%] md:-bottom-28'
-          width={700}
+          className='absolute -bottom-5 left-1/2 translate-x-[-50%] md:-bottom-16'
+          width={600}
           src='/public/asset/high-school/bro.png'
           alt=''
         />
