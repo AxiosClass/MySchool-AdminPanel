24234e7bc2d4e9b1c3cfc841228c3d8a45a4f91f
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2024-10-19 17:11:13 +0600

    login with hasura implemented

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const HASURA_HTTP_URI = 'https://my-school.hasura.app/v1/graphql';
+export const HASURA_ADMIN_SECRET =
+  'i80daRy9KCNaOAeCIJEF0sXrhfsE1zl43j8LEKgrCbqNwFOzyiuCIEHO060tmp8L';
+export const HASURA_WS_LINK = 'wss://my-school.hasura.app/v1/graphql';
--- src/helpers/try-catch.ts
+++ src/helpers/try-catch.ts
@@ -1,6 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { toast } from 'sonner';
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
 interface IArgs {
   id: string | number;
   tryFn: () => void;
