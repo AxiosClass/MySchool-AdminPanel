469cfb91e3235a20a93b3705b08e4cbbce782664
dev-faisal-ahmed <ost.faisal.ahmed>
2024-10-20 13:56:19 +0600

    class creating implemented

--- src/pages/classes/create-class/index.tsx
+++ src/pages/classes/create-class/index.tsx
@@ -7,17 +7,22 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 
-import { Button } from '@/components/ui/button';
-import { FaPlus } from 'react-icons/fa6';
-import { Form } from '@/components/ui/form';
 import { useCreateClass } from './useCreateClass';
+import { Button } from '@/components/ui/button';
 import { TextInput } from '@/components/shared';
+import { Form } from '@/components/ui/form';
+import { FaPlus } from 'react-icons/fa6';
 
 export function CreateClass() {
-  const { form, handleCreateClass } = useCreateClass();
+  const {
+    form,
+    states: { isDialogOpen, isLoading },
+    handlers: { handleCreateClass },
+    setters: { setIsDialogOpen },
+  } = useCreateClass();
 
   return (
-    <Dialog>
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <DialogTrigger asChild>
         <Button className='gap-3'>
           <FaPlus />
@@ -45,7 +50,9 @@ export function CreateClass() {
               name='level'
               placeholder='Level'
             />
-            <Button className='mt-4'>Create Class</Button>
+            <Button disabled={isLoading} className='mt-4'>
+              {isLoading ? 'Create Class' : 'Class Creating...'}
+            </Button>
           </form>
         </Form>
       </DialogContent>
--- src/pages/classes/create-class/useCreateClass.ts
+++ src/pages/classes/create-class/useCreateClass.ts
@@ -1,5 +1,15 @@
+import {
+  CREATE_CLASS,
+  ICreateClassArgs,
+  ICreateClassResponse,
+} from './createClass.query';
+
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@apollo/client';
 import { useForm } from 'react-hook-form';
+import { tryCatch } from '@/helpers';
+import { useState } from 'react';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
 const createClassSchema = z.object({
@@ -8,14 +18,36 @@ const createClassSchema = z.object({
 });
 
 export const useCreateClass = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
   const form = useForm<z.infer<typeof createClassSchema>>({
     resolver: zodResolver(createClassSchema),
     defaultValues: { name: '', level: '' },
   });
 
+  const [createClass, { loading }] = useMutation<
+    ICreateClassResponse,
+    ICreateClassArgs
+  >(CREATE_CLASS);
+
   const handleCreateClass = form.handleSubmit(async (formData) => {
-    console.log(formData);
+    const id = toast.loading('Class creating ...!');
+    const { name, level } = formData;
+
+    tryCatch({
+      id,
+      async tryFn() {
+        await createClass({ variables: { name, level } });
+        toast.success('Class created', { id });
+        setIsDialogOpen(false);
+      },
+    });
   });
 
-  return { form, handleCreateClass };
+  return {
+    form,
+    states: { isDialogOpen, isLoading: loading },
+    handlers: { handleCreateClass },
+    setters: { setIsDialogOpen },
+  };
 };
