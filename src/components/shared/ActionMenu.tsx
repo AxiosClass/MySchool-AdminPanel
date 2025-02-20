5e0e1a69d7b262a7ae1a7e8dc387e50c5c099a1d
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2025-02-21 00:23:23 +0600

    pagination and action menu added

--- src/components/shared/ActionMenu.tsx
+++ src/components/shared/ActionMenu.tsx
@@ -1,3 +1,26 @@
-export const ActionMenu = () => {
-  return <></>
-}
+import { ReactNode } from 'react';
+import { BsThreeDots } from 'react-icons/bs';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from '../ui/dropdown-menu';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
+
+export const ActionMenu = ({ open, onOpenChange, children, className }: TActionMenuProps) => (
+  <DropdownMenu open={open} onOpenChange={onOpenChange}>
+    <DropdownMenuTrigger asChild>
+      <Button variant='ghost'>
+        <BsThreeDots />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent className='min-w-60'>
+      <DropdownMenuLabel className='text-center'>Actions</DropdownMenuLabel>
+      <div className={cn('flex flex-col gap-2', className?.childrenContainer)}>{children}</div>
+    </DropdownMenuContent>
+  </DropdownMenu>
+);
+
+type TActionMenuProps = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  children: ReactNode;
+  className?: { childrenContainer?: string };
+};
--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -6,3 +6,22 @@ export const usePopupState = () => {
 
   return { open, onOpenChange };
 };
+
+export const usePagination = ({ totalPage = 1 }: { totalPage: number | undefined }) => {
+  const [page, setPage] = useState(1);
+
+  const goToPage = useCallback((page: number) => setPage(page), []);
+
+  const goNextPage = useCallback(() => {
+    if (hasNextPage) goToPage(page + 1);
+  }, [page]);
+
+  const goPrevPage = useCallback(() => {
+    if (hasPrevPage) goToPage(page - 1);
+  }, [page]);
+
+  const hasNextPage = page < totalPage;
+  const hasPrevPage = page > 1;
+
+  return { page, goToPage, goNextPage, goPrevPage, hasNextPage, hasPrevPage };
+};
