58479887dc7c2a829ab09038ee758a028dd98217
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2025-05-03 00:33:52 +0600

    added IssueNfcCard form

---
+++ src/pages/students/StudentTable.tsx
@@ -0,0 +1,111 @@
+import moment from 'moment';
+
+import { useQuery } from '@tanstack/react-query';
+import { QK } from '@/api';
+import { getStudents } from '@/api/query';
+import { TableLoader } from '@/components/loader';
+import { CommonTable } from '@/components/shared/CommonTable';
+import { TableCell, TableHead, TableRow } from '@/components/ui/table';
+import { Message, UserIcon } from '@/components/shared';
+import { dateFormatString } from '@/data';
+import { Button } from '@/components/ui/button';
+import { usePopupState } from '@/hooks';
+import { CommonFormField, FormDialog } from '@/components/shared/form';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+export const StudentTable = () => {
+  const { data: students, isLoading } = useQuery({
+    queryKey: [QK.STUDENT],
+    queryFn: getStudents,
+    select: (res) => res.data,
+  });
+
+  if (isLoading) return <TableLoader />;
+  if (!students?.length) return <Message message='No student found' />;
+
+  return (
+    <CommonTable
+      head={
+        <>
+          <TableHead>Student Info</TableHead>
+          <TableHead>Class</TableHead>
+          <TableHead>Address</TableHead>
+          <TableHead>Guardian</TableHead>
+          <TableHead>Admitted At</TableHead>
+          <TableHead className='text-center'>Action</TableHead>
+        </>
+      }
+      className={{ tableContainer: 'px-6' }}
+    >
+      {students.map(({ id, name, class: classInfo, classroom, guardian, address, admittedAt }) => {
+        return (
+          <TableRow key={id} className='border-b'>
+            <TableCell>
+              <div className='flex gap-4'>
+                <UserIcon username={name} />
+                <div>
+                  <p className='text-base font-semibold'>{name}</p>
+                  <p className='text-sm text-muted-foreground'>ID : {id}</p>
+                </div>
+              </div>
+            </TableCell>
+            <TableCell>
+              <p className='text-base font-semibold'>Class : {classInfo}</p>
+              <p className='text-sm text-muted-foreground'>Section : {classroom.name}</p>
+            </TableCell>
+            <TableCell>{address}</TableCell>
+            <TableCell>
+              <p className='text-base font-semibold'> {guardian.name}</p>
+              <p className='text-sm text-muted-foreground'>Cell : {guardian.phone}</p>
+            </TableCell>
+            <TableCell>{moment(admittedAt).format(dateFormatString.basic)}</TableCell>
+            <TableCell>
+              <IssueNfcCard studentId={id} />
+            </TableCell>
+          </TableRow>
+        );
+      })}
+    </CommonTable>
+  );
+};
+
+const IssueNfcCard = ({ studentId, cardId }: TIssueNfcCardProps) => {
+  const formId = 'ISSUE_NFC_CARD_' + studentId;
+  const { open, onOpenChange } = usePopupState();
+
+  const form = useForm<TIssueNfcCardForm>({
+    resolver: zodResolver(issueNfcCardSchema),
+    defaultValues: { cardId: cardId || '' },
+  });
+
+  return (
+    <div className='mx-auto w-fit'>
+      <Button onClick={() => onOpenChange(true)}>Issue Nfc Card</Button>
+      <FormDialog
+        formId={formId}
+        title='Issue Nfc Card'
+        open={open}
+        onOpenChange={onOpenChange}
+        submitButtonTitle='Issue'
+        submitLoadingTitle='Issuing...'
+      >
+        <Form {...form}>
+          <CommonFormField control={form.control} name='cardId'>
+            {({ field }) => <Input {...field} />}
+          </CommonFormField>
+        </Form>
+      </FormDialog>
+    </div>
+  );
+};
+
+// schemas
+const issueNfcCardSchema = z.object({ cardId: z.string().min(1, 'CardId is required') });
+
+// types
+type TIssueNfcCardProps = { studentId: string; cardId?: string };
+type TIssueNfcCardForm = z.infer<typeof issueNfcCardSchema>;
--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -1,16 +1,7 @@
-import { Message, PageHeader } from '@/components/shared';
+import { PageHeader } from '@/components/shared';
 import { PageTitle } from '@/components/shared/PageTitle';
 import { AddStudent } from './AddStudent';
-import { useQuery } from '@tanstack/react-query';
-import { QK } from '@/api';
-import { getStudents } from '@/api/query';
-import { TableLoader } from '@/components/loader';
-import { CommonTable } from '@/components/shared/CommonTable';
-import { TableCell, TableHead, TableRow } from '@/components/ui/table';
-import { UserIcon } from '@/components/shared';
-
-import moment from 'moment';
-import { dateFormatString } from '@/data';
+import { StudentTable } from './StudentTable';
 
 export default function StudentsPage() {
   return (
@@ -23,55 +14,3 @@ export default function StudentsPage() {
     </>
   );
 }
-
-const StudentTable = () => {
-  const { data: students, isLoading } = useQuery({
-    queryKey: [QK.STUDENT],
-    queryFn: getStudents,
-    select: (res) => res.data,
-  });
-
-  if (isLoading) return <TableLoader />;
-  if (!students?.length) return <Message message='No student found' />;
-
-  return (
-    <CommonTable
-      head={
-        <>
-          <TableHead>Name</TableHead>
-          <TableHead>Class</TableHead>
-          <TableHead>Address</TableHead>
-          <TableHead>Guardian</TableHead>
-          <TableHead className='text-right'>Admitted At</TableHead>
-        </>
-      }
-      className={{ tableContainer: 'px-6' }}
-    >
-      {students.map(({ id, name, class: classInfo, classroom, guardian, address, admittedAt }) => {
-        return (
-          <TableRow key={id} className='border-b'>
-            <TableCell>
-              <div className='flex gap-4'>
-                <UserIcon username={name} />
-                <div>
-                  <p className='text-base font-semibold'>{name}</p>
-                  <p className='text-sm text-muted-foreground'>ID : {id}</p>
-                </div>
-              </div>
-            </TableCell>
-            <TableCell>
-              <p className='text-base font-semibold'>Class : {classInfo}</p>
-              <p className='text-sm text-muted-foreground'>Section : {classroom.name}</p>
-            </TableCell>
-            <TableCell>{address}</TableCell>
-            <TableCell>
-              <p className='text-base font-semibold'> {guardian.name}</p>
-              <p className='text-sm text-muted-foreground'>Cell : {guardian.phone}</p>
-            </TableCell>
-            <TableCell className='text-right capitalize'>{moment(admittedAt).format(dateFormatString.basic)}</TableCell>
-          </TableRow>
-        );
-      })}
-    </CommonTable>
-  );
-};
