bdde76473a2b605891d6181bb3818198d6c71082
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2025-05-31 16:07:38 +0600

    added add term functionality

--- src/pages/terms/AddTerm.tsx
+++ src/pages/terms/AddTerm.tsx
@@ -1,19 +1,39 @@
+import { z } from 'zod';
+import { memo } from 'react';
 import { QK } from '@/api';
-import { CommonFormField, FormDialog } from '@/components/shared/form';
-import { ActionButton } from '@/components/ui/button';
+import { toast } from 'sonner';
+import { addTerm } from '@/api/query';
+import { useForm } from 'react-hook-form';
 import { Form } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { ActionButton } from '@/components/ui/button';
+import { CommonFormField, FormDialog } from '@/components/shared/form';
 import { usePopupState } from '@/hooks';
+import { errorMessageGen } from '@/helpers';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { memo } from 'react';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
 
 const formId = QK.TERM + '_ADD';
 
 const AddTerm = memo(() => {
   const { open, onOpenChange } = usePopupState();
+
+  const qc = useQueryClient();
   const form = useForm<TAddTermForm>({ resolver: zodResolver(addTermSchema) });
+
+  const { mutate } = useMutation({
+    mutationKey: [formId],
+    mutationFn: addTerm,
+    onSuccess: (res) => {
+      toast.success(res.message);
+      qc.invalidateQueries({ queryKey: [QK.TERM] });
+      form.reset();
+      onOpenChange(false);
+    },
+    onError: (error) => toast.error(errorMessageGen(error)),
+  });
+
+  const onAddTerms = form.handleSubmit((formData) => mutate({ ...formData }));
 
   return (
     <>
@@ -28,7 +48,7 @@ const AddTerm = memo(() => {
         submitLoadingTitle='Adding...'
       >
         <Form {...form}>
-          <form id={formId}>
+          <form id={formId} onSubmit={onAddTerms}>
             <CommonFormField control={form.control} name='name' label='Name'>
               {({ field }) => <Input placeholder='Enter name' {...field} />}
             </CommonFormField>
