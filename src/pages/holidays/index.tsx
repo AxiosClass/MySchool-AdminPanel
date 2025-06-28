b4e6fd8c6d24cd26c4df77413a42f59001fbb149
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2025-06-29 03:38:45 +0600

    fix: update server address in apiUrl and improve holidays page component structure; enhance loading and no data handling

--- src/pages/holidays/index.tsx
+++ src/pages/holidays/index.tsx
@@ -1,5 +1,5 @@
-import { TableLoader } from '@/components/loader';
-import { CommonTable, Message, PageHeader, PageTitle } from '@/components/shared';
+import { TableBodyLoader } from '@/components/loader';
+import { CommonTable, PageHeader, PageTitle, TableNoData } from '@/components/shared';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { TableCell, TableHead, TableRow } from '@/components/ui/table';
 import { UserIcon } from '@/components/shared/UserIcon';
@@ -7,7 +7,6 @@ import { useQuery } from '@tanstack/react-query';
 import { getHolidays } from '@/api/query';
 import { QK } from '@/api/queryKeys';
 import { AddHoliday } from './AddHoliday';
-import { useMemo } from 'react';
 import { DeleteHoliday } from './DeleteHoliday';
 
 export default function HolidaysPage() {
@@ -24,49 +23,49 @@ export default function HolidaysPage() {
   );
 }
 
-const HolidaysTable = () => {
+const HolidaysTable = () => (
+  <CommonTable head={<HolidaysTableHeader />} tableContainerClassName='px-6'>
+    <HolidaysTableBody />
+  </CommonTable>
+);
+
+const HolidaysTableHeader = () => (
+  <>
+    <TableHead>Holiday name</TableHead>
+    <TableHead>Start date</TableHead>
+    <TableHead>End date</TableHead>
+    <TableHead className='text-center'>Action</TableHead>
+  </>
+);
+
+const HolidaysTableBody = () => {
   const { data: holidaysData, isLoading } = useQuery({
     queryKey: [QK.HOLIDAY],
     queryFn: getHolidays,
     select: (res) => res.data,
   });
 
-  const tableHead = useMemo(() => {
-    return (
-      <>
-        <TableHead>Holiday name</TableHead>
-        <TableHead>Start date</TableHead>
-        <TableHead>End date</TableHead>
-        <TableHead className='text-center'>Action</TableHead>
-      </>
-    );
-  }, []);
+  if (isLoading) return <TableBodyLoader cols={4} />;
+  if (!holidaysData?.length) return <TableNoData colSpan={4} message='No holiday found' />;
 
-  if (isLoading) return <TableLoader />;
-  if (!holidaysData?.length) return <Message message='No holiday Found' />;
-
-  return (
-    <CommonTable head={tableHead} tableContainerClassName='px-6'>
-      {holidaysData.map(({ id, name, description, startDate, endDate }) => (
-        <TableRow className='border-b' key={id}>
-          <TableCell>
-            <div className='flex gap-4'>
-              <UserIcon username={name} />
-              <div>
-                <p className='text-base font-semibold'>{name}</p>
-                <p className='text-sm text-muted-foreground'>{description || 'No description'}</p>
-              </div>
-            </div>
-          </TableCell>
-          <TableCell>{new Date(startDate).toDateString()}</TableCell>
-          <TableCell>{new Date(endDate).toDateString()}</TableCell>
-          <TableCell>
-            <div className='flex items-center justify-center'>
-              <DeleteHoliday holidayId={id} />
-            </div>
-          </TableCell>
-        </TableRow>
-      ))}
-    </CommonTable>
-  );
+  return holidaysData.map(({ id, name, description, startDate, endDate }) => (
+    <TableRow className='border-b' key={id}>
+      <TableCell>
+        <div className='flex gap-4'>
+          <UserIcon username={name} />
+          <div>
+            <p className='text-base font-semibold'>{name}</p>
+            <p className='text-sm text-muted-foreground'>{description || 'No description'}</p>
+          </div>
+        </div>
+      </TableCell>
+      <TableCell>{new Date(startDate).toDateString()}</TableCell>
+      <TableCell>{new Date(endDate).toDateString()}</TableCell>
+      <TableCell>
+        <div className='flex items-center justify-center'>
+          <DeleteHoliday holidayId={id} />
+        </div>
+      </TableCell>
+    </TableRow>
+  ));
 };
